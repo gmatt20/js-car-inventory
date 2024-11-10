cb822bca7c083e7283dc8c6333955145da37a846
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-09 19:47:05 -0800

    Added trim and transmission as basic vehicle info. Added HTLM content to style the page

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function car(make, model, year, miles, price) {
+function car(make, model, year, trim, transmission, miles, price) {
   return {
     make: make,
     model: model,
     year: year,
+    trim: trim,
+    transmission: transmission,
     miles: miles,
     price: price,
     display: function () {
       console.log(
-        `${this.make}, ${this.model}, ${this.year}, ${this.miles}, ${this.price}`
+        `${this.make}, ${this.model}, ${this.year}, ${this.trim}, ${this.transmission}, ${this.miles}, ${this.price}`
       );
     },
     displaySpecs: function carSpecs(engine, horsepower, MPG) {
@@ -17,5 +19,5 @@ function car(make, model, year, miles, price) {
   };
 }
 
-const honda = car("Honda", "Fit", 2014, 138000, 10000);
+const honda = car("Honda", "Fit", 2014, "LX CVT", "Automatic", 138000, 10000);
 honda.displaySpecs("Inline V4", 130, 36);
